e1eec614dbe0914591130d57fde133e886802e41
yanfi-sher <loss.soul20@gmail.com>
2025-01-16 15:56:44 +0200

    cw 16/01

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-console.log("Hello JS")
+// numbers
+// var
+// let
+// const
 
-let a = 3
-a *= 2
+console.log("Hello JS");
 
-console.log(a)
+let a = 10;
+a = 10 / 3;
+function square(name){return name**2 };
+a = square(a);
+console.log(a);
+
+a = Math.trunc(a);
+console.log(a);
+
+a/= 3;
+console.log(a);
+a += "";
+console.log(a);
+let b = 0;
+console.log(b+a);
+a = parseInt(a);
+a = Math.round(a);
+console.log(a);
+a = a.toString();
+
