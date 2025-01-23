2d9f54490dc4d8186c435773491ef4d4d5d060d7
yanfi-sher <loss.soul20@gmail.com>
2025-01-23 18:42:44 +0200

    cw 23/01

--- objects.mjs
+++ objects.mjs
@@ -31,3 +31,5 @@ export default function getOccurencesObject(str) {
     return res;
    
 }
+
+
--- tests/objects.test.mjs
+++ tests/objects.test.mjs
@@ -1,39 +1,63 @@
-import {test, expect} from 'vitest';
-import  getOccurencesObject  from '../objects.mjs';
-test("getOccurencesObject with destructuring", () => {
-    const str = "aaabgbgc";
-    let {a, b, g, c} = getOccurencesObject(str);
-    expect(a).toBe(3);
-    expect(b).toBe(2);
-    expect(g).toBe(2);
-    expect(c).toBe(1);
+// import {test, expect} from 'vitest';
+// import  getOccurencesObject  from '../objects.mjs';
+// test("getOccurencesObject with destructuring", () => {
+//     const str = "aaabgbgc";
+//     let {a, b, g, c} = getOccurencesObject(str);
+//     expect(a).toBe(3);
+//     expect(b).toBe(2);
+//     expect(g).toBe(2);
+//     expect(c).toBe(1);
 
 
-})
-test("string with digits, spaces and hyphens", () => {
-    const str = "1,d-     ";
-    const res = getOccurencesObject(str);
-    expect(res.d).toBe(1);
-    expect(res[1]).toBe(1);
-    expect(res[' ']).toBe(5)
-})
-test ("test for oject as key inside another object", () => {
-    const x = {x:5};
-    x.toString = function() {
-        return `x:${this.x}` //"x:" + this.x
-    };
-    const y = {y:10};
-    const obj1 = {}
-    obj1[x] = 200;
-    const obj2 = obj1;
-    obj2[y] = 300;
-    expect(obj2[x]).toBe(200);
-    expect(obj1["[object Object]"]).toBe(300);
-    expect(obj1[{z:100}]).toBe(300);
-    console.log("printing object using log",x);
-    console.log("printing object using method toSTring",  x.toString());
+// })
+// test("string with digits, spaces and hyphens", () => {
+//     const str = "1,d-     ";
+//     const res = getOccurencesObject(str);
+//     expect(res.d).toBe(1);
+//     expect(res[1]).toBe(1);
+//     expect(res[' ']).toBe(5)
+// })
+// test ("test for oject as key inside another object", () => {
+//     const x = {x:5};
+//     x.toString = function() {
+//         return `x:${this.x}` //"x:" + this.x
+//     };
+//     const y = {y:10};
+//     const obj1 = {}
+//     obj1[x] = 200;
+//     const obj2 = obj1;
+//     obj2[y] = 300;
+//     expect(obj2[x]).toBe(200);
+//     expect(obj1["[object Object]"]).toBe(300);
+//     expect(obj1[{z:100}]).toBe(300);
+//     console.log("printing object using log",x);
+//     console.log("printing object using method toSTring",  x.toString());
     
   
   
     
+// });
+
+import {describe, expect, it} from 'vitest';
+describe ("Object copying", () => {
+    
+   it("showing example of references assignment but not copy", () => {
+    const person1 = {name: "Vasya", age:25};
+     const person2 = person1;
+    person2.gender="male";
+    expect(person1.gender).toBe("male")
+
+   });
+   
+   it("copying using spread operator", () => {
+    const person1 = {name: "Vasya", age:25};
+    const person2 = {...person1}
+    person2.city="Lod";
+    expect(person1.city).toBeUndefined();
+    person1.city="Lod";
+    expect(person1 != person2).toBeTruthy();
+    expect(person2).toEqual(person1);
+    expect(person2).not.toBe(person1)
+   });
+  
 });
