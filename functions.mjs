079a61608ea1f9e0baaff6b00e28099d07156675
yanfi-sher <loss.soul20@gmail.com>
2025-01-26 18:43:21 +0200

    hw 25/01

--- functions.mjs
+++ functions.mjs
@@ -14,10 +14,23 @@
 // display(40)
 
 
-export function myBind(thisArg) {
-    const sourceFun = this;
-    const res =  function() {
-        return sourceFun.apply(thisArg, arguments)
+// export function myBind(thisArg) {
+//     const sourceFun = this;
+//     const res =  function() {
+//         return sourceFun.apply(thisArg, arguments)
+//     }
+//     return res;
+// }
+
+
+export function myBind(thisArg, ...bindArgs) {
+    //write method myBind fully running standard "bind" method functionality
+    //no using standard call / apply methods
+    return (...args) => {
+       const fieldTime = new Date().getTime();
+       thisArg[fieldTime] = this;
+       const res = thisArg[fieldTime](...args, ...bindArgs);
+       delete thisArg[fieldTime];
+       return res;
     }
-    return res;
-}
+ }
